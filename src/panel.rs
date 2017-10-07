b78c058ffbabd1383d398f3dce20053b4357b649
Adam Mayer <phooky@gmail.com>
2017-10-06 22:07:43 -0400

    panel now displays text

--- src/panel.rs
+++ src/panel.rs
@@ -14,6 +14,8 @@ pub struct Panel {
     size : (i32, i32),
     char_cell_px : (i32, i32),
     char_dim : (i32, i32),
+    contents : Vec<u8>,
+    contents_tex : GLuint,
     program : GLuint,
 }
 
@@ -25,6 +27,7 @@ const INDICES : [GLuint; 6] = [
 impl Panel {
 	pub fn new(panel_width_px : u32, size : (i32, i32) ) -> Panel {
         let mut texture : GLuint = 0;
+        let mut contents_tex : GLuint = 0;
         let bytes = include_bytes!("fonts/Osborne_I.charrom");
         let mut vbo : GLuint = 0;
         let mut ebo : GLuint = 0;
@@ -63,6 +66,13 @@ impl Panel {
                            1024, 10, 0,
                            gl::RED_INTEGER,gl::UNSIGNED_BYTE, 
                            bytes.as_ptr() as *const _);
+            gl::GenTextures(1, &mut contents_tex);
+            gl::ActiveTexture(gl::TEXTURE3);
+            gl::BindTexture(gl::TEXTURE_2D, contents_tex);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::NEAREST as i32);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::NEAREST as i32);            
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_BASE_LEVEL, 0);
+            gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAX_LEVEL, 0);
         }
 
 		let mut p = Panel {
@@ -72,6 +82,8 @@ impl Panel {
 			char_cell_px : (8,10),
 			char_dim : (0,0),
 			size : (0,0),
+			contents : Vec::new(),
+			contents_tex : contents_tex,
 			program : program,
 		};
 		p.update_size(size);
@@ -103,6 +115,7 @@ impl Panel {
         	x0, -1.0,   u0, v1,
         ];
         //println!("x1 {} y1 {} x2 {} y2 {}",u0,v0,u1,v1);
+        self.contents.resize( (n*m) as usize, 32);
         unsafe {
             gl::UseProgram(self.program);
             gl::Uniform4f(glutil::uniloc(self.program,"bounds"), 0.0,0.0,n as GLfloat,m as GLfloat);
@@ -119,8 +132,14 @@ impl Panel {
             gl::UseProgram(self.program);
             gl::Uniform4f(glutil::uniloc(self.program,"in_color"), 0.0,1.0,0.0,0.9);
             gl::Uniform1i(glutil::uniloc(self.program,"tex"), 2);
-            gl::ActiveTexture(gl::TEXTURE2);
+            gl::Uniform1i(glutil::uniloc(self.program,"characters"), 3);
             gl::BindVertexArray(self.vao);
+            gl::ActiveTexture(gl::TEXTURE3);
+            gl::BindTexture(gl::TEXTURE_2D, self.contents_tex);
+            gl::TexImage2D(gl::TEXTURE_2D, 0, gl::R8UI as GLint,
+                           self.char_dim.0, self.char_dim.1, 0,
+                           gl::RED_INTEGER,gl::UNSIGNED_BYTE, 
+                           self.contents.as_ptr() as *const _);
             gl::BindBuffer(gl::ARRAY_BUFFER, self.vbo);
             gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null());
         }
